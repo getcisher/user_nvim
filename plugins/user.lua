54c8f67919e0d4fbe49e069816105f8593aead00
Simon Ray <getcisher@gmail.com>
2023-04-13 10:49:53 +0700

    update keys

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,6 +1,43 @@
+-- TODO: TODO
+-- NOTE: NOTE
+-- FIX: FIX
+-- BUG: BUG
+-- HACK: HACK
+-- WARNING: WARN
+-- WARN: test don
+-- TEST: Testing
+-- PERF: FC
+--
 return {
   -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opt = {},
+    event = "User AstroFile",
+    config = function()
+      require("todo-comments").setup {
+        keywords = {
+          FIX = {
+            icon = " ",                              -- icon used for the sign, and in search results
+            color = "error",                            -- can be a hex color, or a named color (see below)
+            alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+            -- signs = false, -- configure signs for some keywords individually
+          },
+          NOTE = { icon = " ", color = "#ffffff", alt = { "INFO" } },
+          TODO = { icon = " ", color = "#0362E0" },
+          HACK = { icon = " ", color = "#b21919" },
+          WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+          PERF = { icon = " ", color = "#91ffee", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+          TEST = { icon = " ", color = "#c0cc81", alt = { "TESTING", "PASSED", "FAILED" } },
+        },
+      }
+    end,
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope keywords=TODO,FIX<cr>" }
+    }
+  },
   -- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",
